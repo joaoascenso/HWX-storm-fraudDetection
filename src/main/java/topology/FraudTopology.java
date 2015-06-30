5735a86316c792d8350af920b609b8eb410eeff4
Joao Ascenso <joao.ascenso@bigdatapartnership.com>
2015-06-30 23:56:16 +0100

    changed main topoligy and saved old as oldFraudTopology

--- src/main/java/topology/FraudTopology.java
+++ src/main/java/topology/FraudTopology.java
@@ -9,35 +9,55 @@ import backtype.storm.StormSubmitter;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.utils.Utils;
 import bolts.DetectFraudBolt;
-import spouts.RandomLineSpout;
+import bolts.SystemOutEchoBolt;
+//import spouts.RandomLineSpout;
+import storm.kafka.KafkaSpout;
+import storm.kafka.SpoutConfig;
+import storm.kafka.ZkHosts;
 
 /**
  * This is a simple Storm topology.
  */
 public class FraudTopology {
 
+    private static final String SENTENCE_SPOUT_ID = "kafka-spout";
+    private static final String TOPOLOGY_NAME = "fraud-detection-topology";
+    private static final String DETECT_FRAUD_BOLT_ID = "fraud-detection-bolt";
+
+    /**
+    * where all the magic happens
+    **/
     public static void main(String[] args) throws Exception {
 
         TopologyBuilder builder = new TopologyBuilder();
 
-        builder.setSpout("url", new RandomLineSpout(), 10);
-        builder.setBolt("detect", new DetectFraudBolt(), 3).shuffleGrouping("url");
+        int numSpoutExecutors = 1;
+        KafkaSpout kspout = buildKafkaSentenceSpout();
+        DetectFraudBolt fraudBolt = new DetectFraudBolt();
+        builder.setSpout(SENTENCE_SPOUT_ID, kspout, numSpoutExecutors);
+        builder.setBolt(DETECT_FRAUD_BOLT_ID, new SystemOutEchoBolt()).shuffleGrouping(SENTENCE_SPOUT_ID);
+        //builder.setBolt(DETECT_FRAUD_BOLT_ID, fraudBolt, 3).shuffleGrouping(SENTENCE_SPOUT_ID);
 
         Config conf = new Config();
-        conf.setDebug(true);
+        conf.setDebug(false);
+        conf.setNumWorkers(1);
+        StormSubmitter.submitTopology(TOPOLOGY_NAME, conf, builder.createTopology());
+    }
 
-        if (args != null && args.length > 0) {
-            conf.setNumWorkers(3);
+    /**
+    * build a kafka spout using the provided libraries
+    **/
+    private static KafkaSpout buildKafkaSentenceSpout() {
+        
+        String zkHostPort = "localhost:2181";
+        String topic = "flume";
 
-            StormSubmitter.submitTopologyWithProgressBar(args[0], conf, builder.createTopology());
-        }
-        else {
-
-            LocalCluster cluster = new LocalCluster();
-            cluster.submitTopology("test", conf, builder.createTopology());
-            Utils.sleep(10000);
-            cluster.killTopology("test");
-            cluster.shutdown();
-        }
+        String zkRoot = "/kafka-spout";
+        String zkSpoutId = "kafka-fraud-spout";
+        ZkHosts zkHosts = new ZkHosts(zkHostPort);
+        
+        SpoutConfig spoutCfg = new SpoutConfig(zkHosts, topic, zkRoot, zkSpoutId);
+        KafkaSpout kafkaSpout = new KafkaSpout(spoutCfg);
+        return kafkaSpout;
     }
 }
